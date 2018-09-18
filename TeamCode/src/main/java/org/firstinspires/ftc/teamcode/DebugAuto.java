dfeada43247e33df0d77bb4638bde371c4c43d87
joannareese <jdreese2016@gmail.com>
2018-09-18 12:47:29 -0400

    first step regular people, first great leap for Bolts and Nuts 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DebugAuto.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Debug Autonomous", group ="test")
+public class DebugAuto extends LinearOpMode {
+
+    @Override public void runOpMode() {
+        //initialization
+        Navigation nav = new Navigation(this,telemetry);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            nav.updatePos();
+            telemetry.addData("pos",nav.pos);
+            telemetry.addData("debug",nav.telemetryString);
+            telemetry.update();
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamAuto.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Team Autonomous")
+public class TeamAuto extends LinearOpMode {
+
+    @Override public void runOpMode() {
+        //initialization
+        Navigation nav = new Navigation(this,telemetry);
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+            //landing
+
+            //identify cube
+
+            //collect/knock cube while avoiding other items
+
+            //nav to marker deploy
+
+            //deploy marker
+
+            //enter crater
+
+        }
+    }
+}
